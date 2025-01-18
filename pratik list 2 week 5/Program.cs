e138a8ae098856fd76819450687ec5362d13cc29
gokay <gokay@GOKAY>
2025-01-18 03:11:19 +0300

    Add project files.

---
+++ pratik list 2 week 5/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<string> kahveListesi = new List<string>();
+
+        Console.WriteLine("Lütfen 5 adet kahve ismi giriniz:");
+
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"kahve {i + 1}: ");
+            string kahve = Console.ReadLine();
+            kahveListesi.Add(kahve);
+
+        }
+        Console.WriteLine("\ngirilen kahve isimeleri:");
+        foreach (string kahve in kahveListesi)
+        {
+            Console.WriteLine(kahve);
+        }
+    }
+}
